1a470ca732c6a45a42172fb87400d8bc430baa7d
Muzanni <moezanni@gmail.com>
2025-06-03 15:51:37 +0700

    Update kirim.php

--- kirim.php
+++ kirim.php
@@ -18,7 +18,7 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'moezanni@gmail.com';
+    $mail->Username   = 'kioscodelab@gmail.com';
     $mail->Password   = 'njyupmencgcsadzb';
     $mail->SMTPSecure = 'tls';
     $mail->Port       = 587;  
